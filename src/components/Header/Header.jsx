dc3b563ada3b7ec67964eb5c48ae3709f03a4c09
DuongTrungQuoc <quoccccc36@gmai.com>
2025-02-27 16:50:34 +0700

    demo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <>
       <header className="flex h-14 items-center justify-between bg-slate-950 px-8 text-white">
         <div className="flex items-center gap-4 text-[1.4vw] font-bold">
-          <img src="/src/assets/logo/logo.png" className="w-16 sm:w-28" />
+          <img src="./src/assets/logo/logo.png" className="w-16 sm:w-28" />
           <a href="#">Cá</a>
           <a href="#">Tôm</a>
         </div>
